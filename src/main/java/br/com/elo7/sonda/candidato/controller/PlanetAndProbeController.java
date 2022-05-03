8728864e05f4d35c80996b13cb29933dcd975407
Clayder <peterclayder@gmail.com>
2022-05-02 21:15:53 -0300

    refactor: PlanetAndProbeController

--- src/main/java/br/com/elo7/sonda/candidato/controller/PlanetAndProbeController.java
+++ src/main/java/br/com/elo7/sonda/candidato/controller/PlanetAndProbeController.java
@@ -2,25 +2,38 @@ package br.com.elo7.sonda.candidato.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.elo7.sonda.candidato.domain.probemanager.entities.IPlanetEntity;
+import br.com.elo7.sonda.candidato.domain.probemanager.entities.IProbeEntity;
+import br.com.elo7.sonda.candidato.domain.probemanager.entities.PlanetEntity;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.elo7.sonda.candidato.dto.InputDTO;
-import br.com.elo7.sonda.candidato.model.Probe;
+import br.com.elo7.sonda.candidato.dto.PlanetDTO;
 import br.com.elo7.sonda.candidato.service.ProbeService;
 
 @Controller
 @RequestMapping("/planet-with-probes")
 public class PlanetAndProbeController {
-	@Autowired
-	private ProbeService probeService;
+	private final ProbeService probeService;
+
+	private final ModelMapper modelMapper;
+
+	public PlanetAndProbeController(ProbeService probeService, ModelMapper modelMapper) {
+		this.probeService = probeService;
+		this.modelMapper = modelMapper;
+	}
 
 	@PostMapping
-    public ResponseEntity<List<Probe>> register(@RequestBody InputDTO inputDto) {
-		return ResponseEntity.ok(probeService.landProbes(inputDto));        
+    public ResponseEntity<PlanetDTO> register(@RequestBody PlanetDTO planetDto) {
+		IPlanetEntity planet = modelMapper.map(planetDto, PlanetEntity.class);
+		List<IProbeEntity> probeEntities = probeService.landProbes(planet);
+		planet.setProbes(probeEntities);
+
+		PlanetDTO newPlanetDto = modelMapper.map(planet, PlanetDTO.class);
+		return ResponseEntity.ok(newPlanetDto);
     }
 }

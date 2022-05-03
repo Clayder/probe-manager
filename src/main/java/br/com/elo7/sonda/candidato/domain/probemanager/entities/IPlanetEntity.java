022c6d52b48328ce5a696bff347807045599f188
Clayder <peterclayder@gmail.com>
2022-05-02 23:31:31 -0300

    refactor: IPlanetEntity

--- src/main/java/br/com/elo7/sonda/candidato/domain/probemanager/entities/IPlanetEntity.java
+++ src/main/java/br/com/elo7/sonda/candidato/domain/probemanager/entities/IPlanetEntity.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public interface IPlanetEntity {
     List<IProbeEntity> getProbes();
-
     void setProbes(List<IProbeEntity> probes);
+    int getWidth();
+    int getHeight();
+
 }
